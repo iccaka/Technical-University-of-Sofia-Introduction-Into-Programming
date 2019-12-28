a30e9de7c9c16075c7c5f57820369a576647eafd
Hristo Mitsev <dakillaroshun@gmail.com>
2019-12-28 19:08:20 +0200

    Complete the 'PeriodsCommasAndNumbers' problem
    The 'PeriodsCommasAndNumbers' problem has been completely solved.

---
+++ PeriodsCommasAndNumbers/PeriodsCommasAndNumbers/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+
+namespace PeriodsCommasAndNumbers
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int commaCount = 0;
+            int periodCount = 0;
+            int wholeNumCount = 0;
+
+            // Default path: /home/iccaka/Documents/programmingRelatedPrograms/riderProjects/MoodleProblemsPlayground/PeriodsCommasAndNumbers/PeriodsCommasAndNumbers/sampleText.txt
+            Console.WriteLine("File path:");
+            String path = Console.ReadLine();
+            StreamReader sr = new StreamReader(path);
+
+            while (sr.Peek() >= 0)
+            {
+                String line = sr.ReadLine();
+
+                try
+                {
+                    periodCount += line.Split(Convert.ToChar(".")).Length - 1;
+                    commaCount += line.Split(Convert.ToChar(",")).Length - 1;
+                }
+                catch (NullReferenceException nre)
+                {
+                    throw new NullReferenceException();
+                }
+
+                int counter = 0;
+                bool isOnNumber = false;
+
+                while (counter <= line.Length - 1)
+                {
+                    if (isOnNumber)
+                    {
+                        if (line[counter] < 48 || line[counter] > 57)
+                        {
+                            isOnNumber = false;
+                            wholeNumCount++;
+                        }                        
+                    }
+                    if (line[counter] >= 48 && line[counter] <= 57)
+                    {
+                        isOnNumber = true;
+                    }
+
+                    counter++;
+                }
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Commas: " + commaCount);
+            Console.WriteLine("Periods: " + periodCount);
+            Console.WriteLine("Numbers: " + wholeNumCount);
+        }
+    }
+}
